e705d5858f11fb0621ec8df4ebc3c6fae238a250
PatrykSobilo <127145148+PatrykSobilo@users.noreply.github.com>
2025-05-15 23:04:44 +0200

    155 działa
    155 działa

--- src/Framework/Container.php
+++ src/Framework/Container.php
@@ -7,6 +7,8 @@ namespace Framework;
 use ReflectionClass, ReflectionNamedType;
 use Framework\Exceptions\ContainerException;
 
+require_once __DIR__ . '/../App/functions.php';
+
 class Container
 {
   private array $definitions = [];
@@ -14,6 +16,7 @@ class Container
 
   public function addDefinitions(array $newDefinitions)
   {
+    dd($newDefinitions);
     $this->definitions = [...$this->definitions, ...$newDefinitions];
   }
 
@@ -21,9 +24,9 @@ class Container
   {
     $reflectionClass = new ReflectionClass($className);
 
-    if (!$reflectionClass->isInstantiable()) {
-      throw new ContainerException("Class {$className} is not instantiable");
-    }
+    // if (!$reflectionClass->isInstantiable()) {
+    //   throw new ContainerException("Class {$className} is not instantiable");
+    // }
 
     $constructor = $reflectionClass->getConstructor();
 
@@ -43,13 +46,13 @@ class Container
       $name = $param->getName();
       $type = $param->getType();
 
-      if (!$type) {
-        throw new ContainerException("Failed to resolve class {$className} because param {$name} is missing a type hint.");
-      }
+      // if (!$type) {
+      //   throw new ContainerException("Failed to resolve class {$className} because param {$name} is missing a type hint.");
+      // }
 
-      if (!$type instanceof ReflectionNamedType || $type->isBuiltin()) {
-        throw new ContainerException("Failed to resolve class {$className} because invalid param name.");
-      }
+      // if (!$type instanceof ReflectionNamedType || $type->isBuiltin()) {
+      //   throw new ContainerException("Failed to resolve class {$className} because invalid param name.");
+      // }
 
       $dependencies[] = $this->get($type->getName());
     }
@@ -59,9 +62,9 @@ class Container
 
   public function get(string $id)
   {
-    if (!array_key_exists($id, $this->definitions)) {
-      throw new ContainerException("Class {$id} does not exist in container.");
-    }
+    // if (!array_key_exists($id, $this->definitions)) {
+    //   throw new ContainerException("Class {$id} does not exist in container.");
+    // }
 
     if (array_key_exists($id, $this->resolved)) {
       return $this->resolved[$id];
